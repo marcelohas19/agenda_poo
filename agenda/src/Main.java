03836a8d7a1e5da89cc32e618ec78150ef77a9fd
marcelohas19 <marcelo.hsousa19@gmail.com>
2025-05-28 08:58:33 -0300

    alterações no cntroler e no main

--- agenda/src/Main.java
+++ agenda/src/Main.java
@@ -1,4 +1,22 @@
+import controller.*;
+import modell.*;
+import view.*;
 public class Main {
     public static void main(String[] args) throws Exception {
+
+        @Override
+        public void run() {
+                // 1. Cria o Model
+                AgendaModel model = new AgendaModel();
+                
+                // 2. Cria a View
+                AgendaView view = new AgendaView();
+                
+                // 3. Cria o Controller, passando o Model e a View
+                AgendaController controller = new AgendaController(model, view);
+                
+                // 4. Inicia a aplicação (torna a View visível)
+                controller.iniciarAplicacao();
+            }
     }
 }
--- agenda/src/controller/AgendaController.java
+++ agenda/src/controller/AgendaController.java
@@ -15,6 +15,18 @@ public class AgendaController {
         this.model = model;
         this.view = view;
 
+        this.view.setListMoedel(this.model);
+
+        this.view.nomedafuncaoincerir(new AdicionarPessoaListener());
+        this.view.nomedafuncaocinsultar(new ConsultaPessoasListener());
+        this.view.nomedafuncaoalterar(new AlterarPessoaListener());
+        this.view.nomedafuncaoexcluir(new ExcluirPessoaListener());
+        this.view.nomedafuncaolimpar(nw LimpaListener());
+
         
+
+    }
+    public void iniciarAplicacao(){
+        view.setVisible(true);
     }
 }
